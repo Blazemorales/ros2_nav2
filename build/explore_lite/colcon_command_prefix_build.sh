bac6a4ab1c79d87916672c4aa4fd067c523528be
Blazemorales <cunhajp2006@gmail.com>
2026-09-23 12:18:49 -0300

    Fix: Alteração no .bashrc para não matar a simulação

---
+++ build/explore_lite/colcon_command_prefix_build.sh
@@ -0,0 +1,2 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/workspaces/ros2_ws/install/explore_lite_msgs/share/explore_lite_msgs/package.sh"
---
+++ build/explore_lite_msgs/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /workspaces/ros2_ws/explore_lite_msgs
+# Build directory: /workspaces/ros2_ws/build/explore_lite_msgs
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("explore_lite_msgs__py")
+subdirs("explore_lite_msgs__rs")
